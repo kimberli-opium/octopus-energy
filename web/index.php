99e4a3d13721e142a725d4f6b3253e585ce15e56
Polina Tiurina <polina.tiurina@kartenmacherei.de>
2022-11-28 02:44:11 +0100

    show data in web

--- web/index.php
+++ web/index.php
@@ -20,18 +20,30 @@ $fileHeader = $exportService->exportFileHeader();
 <table>
     <tr>
         <th>File Identifier</th>
-        <th>Contact</th>
-        <th>Country</th>
+        <th>Data flow and Version Number</th>
+        <th>From Market Participant Role Code</th>
+        <th>File Identifier</th>
+        <th>From Market Participant Id</th>
+        <th>To Market Participant Role Code</th>
+        <th>To Market Participant Id</th>
+        <th>File creation timestamp</th>
+        <th>Sending Application Id</th>
+        <th>Receiving Application Id</th>
+        <th>Broadcast</th>
+        <th>Test data flag</th>
     </tr>
     <tr>
         <td><?php echo $fileHeader[0]['file_id']; ?></td>
-        <td>Maria Anders</td>
-        <td>Germany</td>
-    </tr>
-    <tr>
-        <td>Centro comercial Moctezuma</td>
-        <td>Francisco Chang</td>
-        <td>Mexico</td>
+        <td><?php echo $fileHeader[0]['data_flow_and_version_number']; ?></td>
+        <td><?php echo $fileHeader[0]['from_market_participant_role_code']; ?></td>
+        <td><?php echo $fileHeader[0]['from_market_participant_id']; ?></td>
+        <td><?php echo $fileHeader[0]['to_market_participant_role_code']; ?></td>
+        <td><?php echo $fileHeader[0]['to_market_participant_role_code']; ?></td>
+        <td><?php echo $fileHeader[0]['creation_timestamp']; ?></td>
+        <td><?php echo $fileHeader[0]['sending_application_id']; ?></td>
+        <td><?php echo $fileHeader[0]['receiving_application_id']; ?></td>
+        <td><?php echo $fileHeader[0]['broadcast']; ?></td>
+        <td><?php echo $fileHeader[0]['test_data_flag']; ?></td>
     </tr>
 </table>
 
